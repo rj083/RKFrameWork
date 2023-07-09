bc5bf6d2a62f433fc8e87c9869f408522fb282c1
radhekrishna <kalsariya083@gmail.com>
2023-07-09 10:41:06 +0530

    Add initial code

---
+++ RkFrameWork/RkFrameWork/File.swift
@@ -0,0 +1,16 @@
+//
+//  File.swift
+//  RkFrameWork
+//
+//  Created by RadheKrishna on 09/07/23.
+//
+
+import Foundation
+
+public class hello {
+    
+    func card() {
+        print("hellow world")
+    }
+    
+}
